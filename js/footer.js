24ffdfdc260ec4b9f77255a7aa136f709e0c8011
yuksels2 <yukselcanyurekli@gmail.com>
2026-03-12 17:00:19 +0300

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     <div>
       <a href="index.html" class="footer-logo">
         Burcu Bortaş
-        <small>Uzman Diyetisyen</small>
+        <small>Diyetisyen</small>
       </a>
       <p class="footer-desc">Sağlığınızı bilim ve sevgiyle yönetmenize eşlik eden bütüncül beslenme danışmanlığı.</p>
     </div>
@@ -20,17 +20,20 @@ document.addEventListener("DOMContentLoaded", function () {
     <div>
       <p class="footer-heading">İletişim</p>
       <ul class="footer-links">
-        <li><a href="mailto:info@burcubortas.com">📧 info@burcubortas.com</a></li>
-        <li><a href="tel:+905xxxxxxxxx">📱 +90 (5xx) xxx xx xx</a></li>
-        <li><a href="#">📍 İstanbul, Türkiye</a></li>
+        <li><a href="mailto:burcubortas@gmail.com">📧 burcubortas@gmail.com</a></li>
+        <li><a href="tel:+905527178643">📱 +90 (552) 717 86 43</a></li>
+        <li><a href="#">📍 Ankara, Türkiye</a></li>
       </ul>
     </div>
     <div class="footer-bottom">
       <p>© 2025 Dyt. Burcu Bortaş. Tüm hakları saklıdır.</p>
       <div class="footer-social">
-        <a href="#" class="social-link">in</a>
-        <a href="#" class="social-link">ig</a>
-        <a href="#" class="social-link">yt</a>
+        <a href="https://www.instagram.com/dyt.burcubortas/" target="_blank" class="social-link" aria-label="Instagram">
+          <i class="fa-brands fa-instagram"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/burcu-borta%C5%9F-202539398/" target="_blank" class="social-link" aria-label="LinkedIn">
+          <i class="fa-brands fa-linkedin-in"></i>
+        </a>
       </div>
     </div>
   </footer>
